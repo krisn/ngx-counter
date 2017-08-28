3373b8e7336abfaf5a5e171a5f63fd05634ef359
Kris Nyunt <kris.nyunt@gmail.com>
2017-08-28 23:34:48 +1000

    Color theme support

--- src/counter.component.ts
+++ src/counter.component.ts
@@ -6,20 +6,24 @@ import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
   template: `
 <div class="ct-wrap ct-anim"
   [class.blink]="blink"
-  [class.border-top]="circuit === 0"
-  [class.border-right]="circuit === 1"
-  [class.border-bottom]="circuit === 2"
-  [class.border-left]="circuit === 3"
   [class.small]="size == 'small'"
   [class.medium]="size == 'medium'"
   [class.big]="size == 'big'"
   [class.large]="size == 'large'"
+  [class.xl]="size == 'xl'"
+  [style.color]="color"
+  [style.background-color]="bgcolor"
+  [style.border-top-color]="circuit === 0 ? getColor() : bgcolor"
+  [style.border-right-color]="circuit === 1 ? getColor() : bgcolor"
+  [style.border-bottom-color]="circuit === 2 ? getColor() : bgcolor"
+  [style.border-left-color]="circuit === 3 ? getColor() : bgcolor"
   >
   <div class="ct-count"
     [class.ct-1x]="size == 'small'"
     [class.ct-2x]="size == 'medium'"
     [class.ct-3x]="size == 'big'"
     [class.ct-4x]="size == 'large'"
+    [class.ct-5x]="size == 'xl'"
     [style.font-family]="font"
   >
     {{countValue}}
@@ -29,14 +33,11 @@ import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
   styles: [`
 .ct-wrap {
   display: inline-block;
-  background-color: #d6f0f9;
+  /*background-color: #d6f0f9;*/
   color: blue;
   border-radius: 50%;
-  border-style: none;
+  border-style: solid;
   border-color: #d6f0f9;
-  border-width: 5px;
-  width: 50px;
-  height: 50px;
 }
 
 .small {
@@ -63,43 +64,34 @@ import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
   height: 70px;
 }
 
+.xl {
+  border-width: 10px;
+  width: 80px;
+  height: 80px;
+}
+
 .ct-anim {
   -webkit-transition: background-color 250ms, border-top-color 250ms, border-bottom-color 250ms, border-right-color 250ms, border-left-color 250ms;
   transition: background-color 250ms, border-top-color 250ms, border-bottom-color 250ms, border-right-color 250ms, border-left-color 250ms;
 }
 
 .blink {
-  background-color: #c0d0f0;
-}
-
-.border-top {
-  /*border-top-color: #0000e0;*/
-  border-top-style: solid;
-}
-
-.border-right {
-  /*border-right-color: #0000e0;*/
-  border-right-style: solid;
-}
-
-.border-bottom {
-  /*border-bottom-color: #0000e0;*/
-  border-bottom-style: solid;
-}
-
-.border-left {
-  /*border-left-color: #0000e0;*/
-  border-left-style: solid;
+  background-color: #fcfcfc !important;
 }
 
 .ct-count {
-  font-family: Arial;
+  font-family: monospace;
   position: relative;
   text-align: center;
   top: 50%;
   -webkit-transform: translateY(-50%);
   -ms-transform: translateY(-50%);
   transform: translateY(-50%);
+}
+
+.ct-5x {
+  font-size: 48px;
+  font-weight: bold;
 }
 
 .ct-4x {
@@ -134,6 +126,9 @@ export class CounterComponent implements ControlValueAccessor, OnInit {
 
   @Output() change = new EventEmitter<number>();
   @Input() font: string = 'monospace';
+  @Input() theme: string;
+  @Input() color: string = 'blue';
+  @Input() bgcolor: string = '#d6f0f9';
   @Input() size: string = 'medium';
   @Input() speed: number = 1000;
   @Input() limit: number;
@@ -156,6 +151,23 @@ export class CounterComponent implements ControlValueAccessor, OnInit {
     this.blink = false;
     this.circuit = -1;
     this.limit = this.limit !== undefined ? this.limit : this.countValue + 10;
+
+    switch (this.theme) {
+      case 'red':
+        this.color = 'red';
+        this.bgcolor = '#fff0f0';
+        break;
+      case 'green':
+        this.color = 'green';
+        this.bgcolor = '#e0fff0';
+        break;
+      case 'gray':
+        this.color = 'gray';
+        this.bgcolor = '#f0f0f0';
+        break;
+      default:
+        break;
+    }
 
     this.timer = setInterval(() => {
       if (this.countValue >= this.limit) {
@@ -187,4 +199,8 @@ export class CounterComponent implements ControlValueAccessor, OnInit {
   }
 
   registerOnTouched() {}
+
+  getColor() {
+    return this.color;
+  }
 }
